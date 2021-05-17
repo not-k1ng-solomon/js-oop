9bc58af2066081130a28768e9e3a7d3b46a191d0
not-k1ng-solomon <useinov.sh.i14@gmail.com>
2021-05-17 16:55:39 +0300

    add hanson for slider

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,5 @@ import Slider from "./module/slider";
 window.addEventListener('DOMContentLoaded',()=>{
     const slider = new Slider('.page','.next');
     slider.render();
+
 });
--- src/js/module/slider.js
+++ src/js/module/slider.js
@@ -1,23 +1,39 @@
 export default class Slider {
-    constructor(page, btnsNext) {
+    constructor(page, btnsNext, timeDeley) {
         this.page = document.querySelector(page);
         this.slides = this.page.children;
         this.btnsNext = document.querySelectorAll(btnsNext);
         this.slideIndex = 1;
+        this.timeDeley = timeDeley;
     }
 
     showSlides(n) {
         if (n > this.slides.length) {
             this.slideIndex = 1;
-        }
-        // console.log(this.slides.length);
-        if (n < 1) {
+        } else if (n < 1) {
             this.slideIndex = this.slides.length;
         }
+
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            }else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch (e) {
+        }
+
 
         this.slides.forEach(slide => {
             slide.style.display = 'none';
         });
+
 
         this.slides[this.slideIndex - 1].style.display = 'block';
     }
@@ -31,6 +47,12 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector('.hanson');
+
+        } catch (e) {
+        }
+
         this.btnsNext.forEach(item => {
             item.addEventListener('click', (e) => {
                 e.preventDefault();
