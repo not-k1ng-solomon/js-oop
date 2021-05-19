c535990be9a28be36502630b675992592a83c639
not-k1ng-solomon <useinov.sh.i14@gmail.com>
2021-05-19 23:24:37 +0300

    add MiniSLider

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,30 @@
 import MainSlider from "./module/slider/slider-main";
+import MiniSlider from "./module/slider/slider-mini";
 import VideoPlayer from "./module/playVideo";
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new MainSlider({page: '.page', btns: '.next'});
+    const slider = new MainSlider({container: '.page', btns: '.next'});
     slider.render();
     const play = new VideoPlayer('.showup .play', '.overlay');
     play.init();
+    const showUpSlider = new MiniSlider({
+        container: '.showup__content-slider',
+        next: '.showup__next',
+        prev: '.showup__prev'
+    });
+    showUpSlider.init();
+
+    const moduleSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        next: '.modules__info-btns .slick-next',
+        prev: '.modules__info-btns .slick-prev'
+    });
+    moduleSlider.init();
+
+    const feedSlider = new MiniSlider({
+        container: '.feed__slider',
+        next: '.feed__slider .slick-next',
+        prev: '.feed__slider .slick-prev'
+    });
+    feedSlider.init();
 });
---
+++ src/js/module/slider/slider-mini.js
@@ -0,0 +1,29 @@
+import Slider from "./slider";
+
+export default class MiniSlider extends Slider {
+    constructor(container, next, prev) {
+        super(container, next, prev);
+    }
+
+    bindTriggers() {
+        this.next.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.container.appendChild(this.slides[0]);
+        });
+        this.prev.addEventListener('click', () => {
+            let active = this.slides[this.slides.length - 1];
+            this.container.insertBefore(active, this.slides[0]);
+            // this.container.appendChild(this.slides[0]);
+        });
+    }
+
+    init() {
+        this.container.style.cssText = `
+            display: flex;
+            flex-wrap: wrap;
+            overflow: hidden;
+            align-items: flex-start;
+        `;
+        this.bindTriggers();
+    }
+}
